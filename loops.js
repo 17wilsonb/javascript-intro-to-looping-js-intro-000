ac85549ca962b7f4626c72ee3653e9f01209d468
Brett Wilson <wilson.3458@osu.edu>
2020-05-13 22:36:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,13 @@
-function forLoop (array){
+function forLoop(array){
   for(let i = 0; i < 25; i++){
     array.push(`I am ${i} strange loop${i === 1 ? '':'s'}.`);
   }
   return array;
+}
+
+function whileLoop(n){
+  while(n >= 0){
+    console.log(n);
+  }
+  console.log('done');
 }
