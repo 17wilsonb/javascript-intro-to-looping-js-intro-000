9d77e1b905b2b7c1b7ed5ce31dcaec9f9fca1466
Brett Wilson <wilson.3458@osu.edu>
2020-05-13 22:34:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop (array){
-  for(let i = 0; i < array.length; i++){
+  for(let i = 0; i < 25; i++){
     array.push(`This is ${i} strange loop${i === 1 ? '':'s'}.`);
   }
   return array;
